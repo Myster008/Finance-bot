d91c89bddc0e92918f78de25576dcdd8c459ea9f
Myster008 <ahrorburhonov0803@gmail.com>
2026-04-28 17:07:47 +0500

    Add progress bar and currency formatting functions

--- MoneyMind_Game/utils/helpers.py
+++ MoneyMind_Game/utils/helpers.py
@@ -1,1 +1,10 @@
+def draw_progress_bar(current, total, length=20):
+    """Sog'liq yoki pul uchun vizual progress bar chizish"""
+    percent = float(current) / total
+    arrow = '-' * int(round(percent * length) - 1) + '>'
+    spaces = ' ' * (length - len(arrow))
+    return f"[{arrow + spaces}] {int(percent * 100)}%"
 
+def format_currency(amount):
+    """Pulni chiroyli formatda ko'rsatish: $1,250.00"""
+    return f"${amount:,.2f}"
